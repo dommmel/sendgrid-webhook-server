22623e1fc50bf0c7da67b7b3bdc48c8dc0fa431c
Larry Silverman <lsilverman@trackabout.com>
2013-04-11 20:36:30 -0500

    Renamed some variables for clarity. Fixed processing of smtp-id.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-This is a simple node server designed to accept webhook requests from SendGrid for relaying to a variety of other destinations. Currently, forwards messages to a HipChat room.
+This is a node server designed to accept webhook requests from SendGrid for relaying to a variety of other destinations. Currently, forwards messages to a HipChat room.
+
+In your SendGrid configuration, look the Event Notification App. It will be disabled by default. Here you can enable the web hook.
+
+Choose to be notified for whichever events you're interested in. We typically monitor: dropped, bounced, unsubscribe, spam.  Monitoring deferred email seemed to create a lot of noise.
 
 Server and routing code was adapted from Manuel Kiessling's book, The Node Beginner Book at http://www.nodebeginner.org.
 
-
+TODO: This code cannot currently handle enabling the "Batch event notifications" setting in SendGrid. The code expects only one message at a time.
--- requestHandlers.js
+++ requestHandlers.js
@@ -19,51 +19,51 @@ function _postMessage(msg, apikey, roomNumber, fromName) {
     client.postMessage(params, function (value) { console.log(value); })
 }
 
-function _formatHipChatMessage(evt, environment)
-{
-    //return util.inspect(evt);
-    var output = printf('Email from <b>%s</b> to <b>%s</b> was <b>%s</b>', environment, evt.email, evt.event);
+function _formatHipChatMessage(eventData, environment) {
+    // Uncomment here to just dump 
+    //return util.inspect(eventData);
+    var output = printf('Email from <b>%s</b> to <b>%s</b> was <b>%s</b>', environment, eventData.email, eventData.event);
 
-    if (evt.status)
-        output += printf('<br>Bounce Status: %s', evt.status);
+    if (eventData.status)
+        output += printf('<br>Bounce Status: %s', eventData.status);
 
-    if (evt.type)
-        output += printf('<br>Bounce Type: %s', evt.type);
+    if (eventData.type)
+        output += printf('<br>Bounce Type: %s', eventData.type);
 
-    if (evt.reason)
-        output += printf('<br>Reason: %s', evt.reason);
+    if (eventData.reason)
+        output += printf('<br>Reason: %s', eventData.reason);
 
-    if (evt.response)
-        output += printf('<br>Response: %s', evt.response);
+    if (eventData.response)
+        output += printf('<br>Response: %s', eventData.response);
 
-    if (evt.url)
-        output += printf('<br>URL: %s', evt.url);
+    if (eventData.url)
+        output += printf('<br>URL: %s', eventData.url);
 
-    if (evt.category)
-        output += printf('<br>Category: %s', evt.category);
+    if (eventData.category)
+        output += printf('<br>Category: %s', eventData.category);
 
-    if (evt.attempt)
-        output += printf('<br>Attempt: %s', evt.attempt);
+    if (eventData.attempt)
+        output += printf('<br>Attempt: %s', eventData.attempt);
 
-    if (evt["smtp-id"] && evt["smtp-id"].length > 0)
-        output += printf('<br>smtp-id: %s', evt["smtp-id"]);
+    // This smtp-id needs to be handled specially because of the dash.
+    // Can't access it like a regular object property because javascript interprets the dash as a minus sign.
+    if ("smtp-id" in eventData && eventData["smtp-id"].length > 0)
+        output += printf('<br>smtp-id: %s', eventData["smtp-id"]);
 
     return output;
 }
 
-function _getLinks(userid)
-{
-    return printf('<a href="http://sendgrid.com/subuser/emailLogs/id/%s">Email Logs</a>&nbsp;&nbsp;',userid)
-     + printf('<a href="http://sendgrid.com/subuser/bounces/id/%s">Bounces</a>&nbsp;&nbsp;',userid)
-     + printf('<a href="http://sendgrid.com/subuser/blocks/id/%s">Blocks</a>&nbsp;&nbsp;',userid)
-     + printf('<a href="http://sendgrid.com/subuser/spamReports/id/%s">Spam Reports</a>&nbsp;&nbsp;',userid)
-     + printf('<a href="http://sendgrid.com/subuser/invalidEmail/id/%s">Invalid Emails</a>&nbsp;&nbsp;',userid)
-     + printf('<a href="http://sendgrid.com/subuser/unsubscribes/id/%s">Unsubscribes</a>',userid)
+function _getLinks(userid) {
+    return printf('<a href="http://sendgrid.com/subuser/emailLogs/id/%s">Email Logs</a>&nbsp;&nbsp;', userid)
+     + printf('<a href="http://sendgrid.com/subuser/bounces/id/%s">Bounces</a>&nbsp;&nbsp;', userid)
+     + printf('<a href="http://sendgrid.com/subuser/blocks/id/%s">Blocks</a>&nbsp;&nbsp;', userid)
+     + printf('<a href="http://sendgrid.com/subuser/spamReports/id/%s">Spam Reports</a>&nbsp;&nbsp;', userid)
+     + printf('<a href="http://sendgrid.com/subuser/invalidEmail/id/%s">Invalid Emails</a>&nbsp;&nbsp;', userid)
+     + printf('<a href="http://sendgrid.com/subuser/unsubscribes/id/%s">Unsubscribes</a>', userid)
 }
 
 function _isInputValid(input, errorMessage, response) {
-    if (typeof input === 'undefined' || !input)
-    {
+    if (typeof input === 'undefined' || !input) {
         response.writeHead(405, { 'Content-Type': 'text/plain' });
         response.end(errorMessage);
         return false;
@@ -71,8 +71,17 @@ function _isInputValid(input, errorMessage, response) {
     return true;
 }
 
+
+/**
+ * Call this endpoint from a SendGrid webhook.
+ * Expects the following querystring parameters:
+ * apikey - HipChat API Key
+ * room - HipChat room number (can be fetched from viewing the chat history of any room)
+ * user - SendGrid UserId (if using sub-accounts, which we are). This is used to make links back to SendGrid reports.
+ * environment - This is an arbitrary string, can contain whatever you want. If you use subusers, you can use this to distinguish which one is calling the webhook.
+ */
 function postSendGridMessage(response, request) {
-    var queryData = "";    
+    var rawPostData = "";
     var url_parts = url.parse(request.url, true);
     var query = url_parts.query;
 
@@ -85,13 +94,13 @@ function postSendGridMessage(response, request) {
         return;
     if (!_isInputValid(query.environment, "SendGrid environment name not provided.", response))
         return;
-    
+
     if (request.method == 'POST') {
         response.writeHead(200, { "Content-Type": "text/html" });
         request.on('data', function (data) {
-            queryData += data;
-            if (queryData.length > 1e6) {
-                queryData = "";
+            rawPostData += data;
+            if (rawPostData.length > 1e6) {
+                rawPostData = "";
                 response.writeHead(413, { 'Content-Type': 'text/plain' });
                 request.connection.destroy();
             }
@@ -99,23 +108,19 @@ function postSendGridMessage(response, request) {
 
         request.on('end', function () {
             response.writeHead(200, { "Content-Type": "text/html" });
-            var evt = querystring.parse(queryData);
-            var output = _formatHipChatMessage(evt, query.environment);
+            console.log(rawPostData);
+            var eventData = querystring.parse(rawPostData);
+            var output = _formatHipChatMessage(eventData, query.environment);
 
             _postMessage(output, query.apikey, query.room, 'SendGrid');
 
+            // Every so many messages, inject links back to SendGrid report pages.
             if (messageCount === 0)
-            {
                 _postMessage(_getLinks(query.user), query.apikey, query.room, 'SendGrid');
-            }
-
-            if (messageCount === (linksEvery - 1))
+            if (messageCount++ === linksEvery)
                 messageCount = 0;
-            else
-                messageCount++;
 
             response.end();
-
         });
     }
     else {
@@ -124,7 +129,10 @@ function postSendGridMessage(response, request) {
     }
 
 }
-
+/**
+ * A url to query to let us know the service is still running.
+ * Simply returns 0.
+ */
 function heartbeat(response, request) {
     response.writeHead(200, { "Content-Type": "text/html" });
     response.end('0');
